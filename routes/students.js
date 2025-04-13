5ade1784a9679e1be327f8d995afdd3a5e34f797
IshwariDole <doleishwari@gmail.com>
2025-04-13 21:27:16 +0530

    Add Student model

--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Student = require('../models/Student');
+const Student = require('../models/student');
 const { sendMessage } = require('../whatsapp/whatsapp');
 const { scheduleMessage } = require('../whatsapp/scheduler');
 
